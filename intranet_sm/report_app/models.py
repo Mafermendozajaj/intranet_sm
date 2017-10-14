1b9ff010b595c7f8fde8bf97debaad37964fca7e
Maria Fernanda Mendoza <mafe_1204@hotmail.com>
2017-10-14 11:18:31 -0400

    14octubre

--- intranet_sm/report_app/models.py
+++ intranet_sm/report_app/models.py
@@ -8,38 +8,47 @@ TIPO_REPORTE_CHOICES = (
     ('C', 'Reporte C'),
 )
 
-class Proyecto (models.Model):	
-    proyecto = models.CharField('Proyecto',max_length=255)
+
+class Proyecto (models.Model):
+    proyecto = models.CharField('Proyecto', max_length=255)
     cod_proyecto = models.CharField(max_length=20)
 
     def __str__(self):
         return u'%s' % (self.proyecto)
 
+
 class Telescopio(models.Model):
-    telescopio = models.CharField('Telescopio',max_length=30)
+    telescopio = models.CharField('Telescopio', max_length=30)
 
     def __str__(self):
         return u'%s' % (self.telescopio)
 
 
-
 class Reporte(models.Model):
     asistente = models.ForeignKey(Empleado)
-    observador = models.CharField('Observador',max_length=100, null=True, blank=True)
+    observador = models.CharField(
+        'Observador', max_length=100, null=True, blank=True)
     proyecto = models.ForeignKey(Proyecto)
-    tipo_reporte = models.CharField('Tipo de reporte', max_length=2,choices=TIPO_REPORTE_CHOICES, default='A')
+    tipo_reporte = models.CharField(
+        'Tipo de reporte', max_length=2, choices=TIPO_REPORTE_CHOICES, default='A')
     telescopio = models.ForeignKey(Telescopio)
     fecha_obs = models.DateField('Fecha de observacion')
     datos = models.BooleanField(default=True)
-    observadores = models.CharField('Observadores', max_length=100, null=True, blank=True, default='', help_text="Introduzca los nombres de los observadores")
+    observadores = models.CharField('Observadores', max_length=100, null=True,
+                                    blank=True, default='', help_text="Introduzca los nombres de los observadores")
     horas_trabajadas = models.IntegerField()
     hp_clima = models.DecimalField('HP Clima', max_digits=4, decimal_places=2)
-    hp_instrumentos = models.DecimalField('HP instrumentos', max_digits=4, decimal_places=2)
-    hp_software = models.DecimalField('HP Software', max_digits=4, decimal_places=2)
+    hp_instrumentos = models.DecimalField(
+        'HP instrumentos', max_digits=4, decimal_places=2)
+    hp_software = models.DecimalField(
+        'HP Software', max_digits=4, decimal_places=2)
     comentarios = models.TextField('Comentarios')
-    vacio_camara = models.DecimalField('Vacio de cámara', max_digits=6, decimal_places=2)
-    vacio_ls = models.DecimalField('Vacio de ls', max_digits=6, decimal_places=2)
-    vacio_li = models.DecimalField('Vacio de li', max_digits=6, decimal_places=2)
+    vacio_camara = models.DecimalField(
+        'Vacio de cámara', max_digits=6, decimal_places=2, default=0.0)
+    vacio_ls = models.DecimalField(
+        'Vacio de ls', max_digits=6, decimal_places=2, default=0.0)
+    vacio_li = models.DecimalField(
+        'Vacio de li', max_digits=6, decimal_places=2, default=0.0)
     enviado = models.BooleanField(default=False)
     fecha_reg = models.DateField('Fecha de registro', auto_now_add=True)
     ult_act = models.DateField('Ultima actualización', auto_now=True)
